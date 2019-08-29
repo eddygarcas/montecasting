053a2553ff331da69d61f1843c22ac59d5101832
Eduard Garcia <edugarcas@gmail.com>
2019-08-29 16:44:22 +0200

    Ruby Gem for Project Forecasting Methods

---
+++ lib/montecasting.rb
@@ -0,0 +1,6 @@
+require "montecasting/version"
+
+module Montecasting
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/montecasting/version.rb
@@ -0,0 +1,3 @@
+module Montecasting
+  VERSION = "0.1.0"
+end
