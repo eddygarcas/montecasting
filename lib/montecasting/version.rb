319cd594153edab5db73c7dbb3557ffbf4413b93
Eduard Garcia <edugarcas@gmail.com>
2020-03-26 16:44:16 +0100

    Update gemfile gems

--- lib/montecasting/version.rb
+++ lib/montecasting/version.rb
@@ -2,5 +2,5 @@ module Montecasting
   # MAJOR version when you make incompatible API changes,
   # MINOR version when you add functionality in a backwards compatible manner, and
   # PATCH version when you make backwards compatible bug fixes.
-  VERSION = "0.6.5"
+  VERSION = "0.7.0"
 end
